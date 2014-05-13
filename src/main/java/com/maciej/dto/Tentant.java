618ee579bbe68175ebe76b9be2daf1f24ddba0bf
maciekTulaza <azalut@gmail.com>
2014-05-13 22:06:45 +0200

    finished modeling Tentant dto object

--- src/main/java/com/maciej/dto/Tentant.java
+++ src/main/java/com/maciej/dto/Tentant.java
@@ -1,10 +1,11 @@
 package com.maciej.dto;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 public class Tentant implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String lastname;
@@ -15,6 +16,76 @@ public class Tentant implements Serializable {
      * Number of identity card of tentant
      * */
     private String idCardNumber;
+    @OneToOne
     private Rent rentId;
 
+    public Tentant(String name, String lastname, String email, String address, String telnumber, String idCardNumber, Rent rentId) {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.address = address;
+        this.telnumber = telnumber;
+        this.idCardNumber = idCardNumber;
+        this.rentId = rentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelnumber() {
+        return telnumber;
+    }
+
+    public void setTelnumber(String telnumber) {
+        this.telnumber = telnumber;
+    }
+
+    public String getIdCardNumber() {
+        return idCardNumber;
+    }
+
+    public void setIdCardNumber(String idCardNumber) {
+        this.idCardNumber = idCardNumber;
+    }
+
+    public Rent getRentId() {
+        return rentId;
+    }
+
+    public void setRentId(Rent rentId) {
+        this.rentId = rentId;
+    }
 }
