78a80d174e220a70fc7953a0cbb4fc4ddd13a269
maciekTulaza <azalut@gmail.com>
2014-05-14 22:09:45 +0200

    did DAOs, started doing HibernateTentantDao(not done - have to finish), configurated spring+hibernate bean in dispatcherServlet-servletxml

---
+++ src/main/java/com/maciej/dao/TentantDao.java
@@ -0,0 +1,13 @@
+package com.maciej.dao;
+
+import com.maciej.dto.Tentant;
+
+import java.util.List;
+
+public interface TentantDao {
+    public Tentant readTentantById(int id);
+    public boolean createTentant(Tentant tentant);
+    public boolean updateTentantById(int id, Tentant tentant);
+    public boolean deleteTentantById(int id);
+    public List<Tentant> getTentantList();
+}
